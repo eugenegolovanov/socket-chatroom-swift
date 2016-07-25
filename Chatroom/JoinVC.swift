bbc05ff265233260769ce98b9e2170025ab69ff2
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-25 00:21:13 -0700

    first commit

---
+++ Chatroom/JoinVC.swift
@@ -0,0 +1,54 @@
+//
+//  JoinVC.swift
+//  Chatroom
+//
+//  Created by eugene golovanov on 7/25/16.
+//  Copyright © 2016 eugene golovanov. All rights reserved.
+//
+
+import UIKit
+
+class JoinVC: UIViewController {
+
+    
+    //---------------------------------------------------------------------------
+    // MARK: - Properties
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var chatroomField: UITextField!
+    
+    
+    
+    //---------------------------------------------------------------------------
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    
+    //---------------------------------------------------------------------------
+    // MARK: - Actions
+
+    
+    @IBAction func joinChatAction(sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    
+    //---------------------------------------------------------------------------
+    // MARK: - Navigation
+
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//    }
+
+
+}
